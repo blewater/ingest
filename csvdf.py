ea0de171a6ba1047bd16d4155ae5f74a117a0db9
Mario Karagiorgas <2580304+blewater@users.noreply.github.com>
2023-04-20 09:32:21 +0300

    Py Linting feedback

--- csvdf.py
+++ csvdf.py
@@ -1,30 +1,43 @@
 import os
 import re
+import sys
+
+from urllib.parse import urlparse
 import tiktoken
 import pandas as pd
 import openai
-from urllib.parse import urlparse
 
-# Create a list to store the text files
+CRAWLED_PAGES = "output/"
 IGNORE_TEST_FILES = False
 
 
-def process_git_folder(git_folder_path):
+def remove_newlines(content):
+    content = content.str.replace('\n', ' ')
+    content = content.str.replace('\\n', ' ')
+    content = content.str.replace('  ', ' ')
+    content = content.str.replace('  ', ' ')
+    return content
+
+
 def process_git_folder(git_folder_path, ignore_test_files=True):
     texts = []
 
     for root, _, files in os.walk(git_folder_path):
         for file in files:
             if file.endswith(".go"):
+                # Ignore test files if the flag is set to True
+                if ignore_test_files and file.endswith("_test.go"):
+                    continue
+
                 file_path = os.path.join(root, file)
 
-                with open(file_path, "r", encoding="UTF-8") as f:
-                    text = f.read()
+                with open(file_path, "r", encoding="UTF-8") as go_file:
+                    go_content = go_file.read()
 
-                    texts.append((file, text))
+                    texts.append((file, go_content))
 
     data_frame = pd.DataFrame(texts, columns=['fname', 'text'])
-    data_frame['text'] = data_frame.fname + ". " + data_frame.text.apply(remove_newlines)
+    data_frame['text'] = data_frame.fname + ". " + remove_newlines(data_frame.text)
 
     return data_frame
 
@@ -32,30 +45,30 @@ def process_git_folder(git_folder_path, ignore_test_files=True):
 def process_website(url):
     domain = urlparse(url).hostname
 
-    # Create a list to store the text files
+    # Create a list to store the web text files
     texts = []
 
-    # Get all the text files in the text directory
-    for file in os.listdir("text/" + domain + "/"):
-        # Open the file and read the text
-        with open("text/" + domain + "/" + file, "r", encoding="UTF-8") as f:
-            text = f.read()
+    # Get all the web text files in the web text directory
+    for file in os.listdir(CRAWLED_PAGES + domain + "/"):
+        # Open the file and read the web text
+        with open(CRAWLED_PAGES + domain + "/" + file, "r", encoding="UTF-8") as web_file:
+            web_text = web_file.read()
 
             # Omit the first 11 lines and the last 4 lines, then replace -, _, and #update with spaces.
-            texts.append((file[11:-4].replace('-', ' ').replace('_', ' ').replace('#update', ''), text))
+            texts.append((file[11:-4].replace('-', ' ').replace('_', ' ').replace('#update', ''), web_text))
 
     # Create a dataframe from the list of texts
     data_frame = pd.DataFrame(texts, columns=['fname', 'text'])
 
-    # Set the text column to be the raw text with the newlines removed
+    # Set the web_text column to be the raw web_text with the newlines removed
     data_frame['text'] = data_frame.fname + ". " + remove_newlines(data_frame.text)
 
     return data_frame
 
 
-def is_url(s):
+def is_url(url):
     pattern = re.compile(r'https?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
-    return bool(pattern.match(s))
+    return bool(pattern.match(url))
 
 
 if len(sys.argv) != 2:
@@ -93,26 +106,20 @@ for line in config_lines:
         continue
 
     if is_url(line):
-        text = process_website(line)
-        df = pd.DataFrame({'fname': [urlparse(line).hostname], 'text': [text]})
+        print("Processing website: " + line + "...")
+        site_text = process_website(line)
+        df = pd.DataFrame({'fname': [urlparse(line).hostname], 'text': [site_text]})
         dfs.append(df)
     else:
-        df = process_git_folder(line)
+        print("Processing Git folder: " + line + "...")
+        df = process_git_folder(line, )
         dfs.append(df)
 
 # Combine all data frames
 combined_df = pd.concat(dfs, ignore_index=True)
 csv_filename = f"processed/{topic}.csv"
-combined_df.head()
-
-
-def remove_newlines(content):
-    content = content.str.replace('\n', ' ')
-    content = content.str.replace('\\n', ' ')
-    content = content.str.replace('  ', ' ')
-    content = content.str.replace('  ', ' ')
-    return content
-
+combined_df.to_csv(csv_filename)
+print(f"Saved combined data frames to {csv_filename}")
 
 # Tokenize
 # Load the cl100k_base tokenizer which is designed to work with the ada-002 model
@@ -127,11 +134,11 @@ df['n_tokens'] = df.text.apply(lambda x: len(tokenizer.encode(x)))
 # Visualize the distribution of the number of tokens per row using a histogram
 df.n_tokens.hist()
 
-max_tokens = 500
+MAX_TOKENS = 500
 
 
 # Function to split the text into chunks of a maximum number of tokens
-def split_into_many(text, max_tokens=max_tokens):
+def split_into_many(text, max_tokens_in=MAX_TOKENS):
     # Split the text into sentences
     sentences = text.split('. ')
 
@@ -148,14 +155,14 @@ def split_into_many(text, max_tokens=max_tokens):
         # If the number of tokens so far plus the number of tokens in the current sentence is greater
         # than the max number of tokens, then add the chunk to the list of chunks and reset
         # the chunk and tokens so far
-        if tokens_so_far + token > max_tokens:
+        if tokens_so_far + token > max_tokens_in:
             chunks.append(". ".join(chunk) + ".")
             chunk = []
             tokens_so_far = 0
 
         # If the number of tokens in the current sentence is greater than the max number of
         # tokens, go to the next sentence
-        if token > max_tokens:
+        if token > max_tokens_in:
             continue
 
         # Otherwise, add the sentence to the chunk and add the number of tokens to the total
@@ -175,7 +182,7 @@ for row in df.iterrows():
         continue
 
     # If the number of tokens is greater than the max number of tokens, split the text into chunks
-    if row[1]['n_tokens'] > max_tokens:
+    if row[1]['n_tokens'] > MAX_TOKENS:
         shortened += split_into_many(row[1]['text'])
 
     # Otherwise, add the text to the list of shortened texts
